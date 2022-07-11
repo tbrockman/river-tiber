79371760a777cc311360c4df06838aa8ff9daff9
theodore brockman <tmanbrock@gmail.com>
2022-07-10 21:18:56 -0700

    Putting everything together, getting ready to deploy test.

---
+++ components/SocialButtons.tsx
@@ -0,0 +1,55 @@
+import styles from './SocialButtons.module.css';
+import { SocialIcon } from 'react-social-icons';
+import { useEffect, useRef, useState } from 'react';
+
+function TiberIcon(props: any) {
+	const [fgColor, setFgColor] = useState('#fff');
+	const [bgColor, setBgColor] = useState('#000');
+
+	const onEnter = () => {
+		setFgColor('#000');
+		setBgColor('#fff');
+	};
+
+	const onLeave = () => {
+		setFgColor('#fff');
+		setBgColor('#000');
+	};
+
+	return (
+		<SocialIcon
+			{...props}
+			bgColor={bgColor}
+			fgColor={fgColor}
+			onMouseEnter={onEnter}
+			onMouseLeave={onLeave}
+		/>
+	);
+}
+
+type SocialButtonsProps = {
+	setSocialButtonsHeight?: (height: number) => any;
+	[rest: string]: any;
+};
+
+export default function SocialButtons({
+	setSocialButtonsHeight,
+	...rest
+}: SocialButtonsProps) {
+	const ref = useRef<HTMLDivElement>();
+
+	useEffect(() => {
+		if (ref.current && setSocialButtonsHeight) {
+			setSocialButtonsHeight(ref.current.offsetHeight);
+		}
+	}, [ref.current]);
+
+	return (
+		<div className={styles['social-buttons']} ref={ref} {...rest}>
+			<TiberIcon url="https://open.spotify.com/artist/1FHDMgGdmIP4IsoFkAwbS0"></TiberIcon>
+			<TiberIcon url="https://www.instagram.com/rivertiber"></TiberIcon>
+			<TiberIcon url="https://twitter.com/rivertiber"></TiberIcon>
+			<TiberIcon url="mailto:bookings@rivertiber.com"></TiberIcon>
+		</div>
+	);
+}
---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import MusicVideo from '../components/MusicVideo';
+import SocialButtons from '../components/SocialButtons';
+import { useState } from 'react';
+
+function FakeHeader({ socialButtonsHeight }) {
+	return (
+		<div style={{ height: socialButtonsHeight, margin: '2rem 0' }}></div>
+	);
+}
+
+export default function FitScreen() {
+	const [socialButtonsHeight, setSocialButtonsHeight] = useState(0);
+
+	return (
+		<div className={`${styles.container} ${styles.app}`}>
+			<Head>
+				<title>river tiber</title>
+				<meta
+					name="description"
+					content="canadian singer, songwriter, multi-instrumentalist and record producer. 👁👁"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main className={styles.main}>
+				<div className={styles.content}>
+					<FakeHeader socialButtonsHeight={socialButtonsHeight} />
+					<MusicVideo />
+					<SocialButtons
+						setSocialButtonsHeight={setSocialButtonsHeight}
+					/>
+				</div>
+			</main>
+		</div>
+	);
+}
