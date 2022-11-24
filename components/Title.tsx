230970282d98c80617c30db08032b50ce23fa9d2
theodore brockman <tmanbrock@gmail.com>
2022-11-23 16:16:41 -0800

    Hopefully fixing build.

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,7 +4,7 @@ import RiverTiberLogo from '../static/river-tiber-logo.svg';
 export default function Title() {
 	return (
 		<div style={{ maxWidth: '50ch', textAlign: 'center' }}>
-			<Image src={RiverTiberLogo} />
+			<Image src={RiverTiberLogo} alt="dreaming eyes" />
 		</div>
 	);
 }
--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
 	reactStrictMode: true,
 	assetPrefix: isProd ? '/river-tiber/' : '',
+	images: {
+		unoptimized: true,
+	},
 };
